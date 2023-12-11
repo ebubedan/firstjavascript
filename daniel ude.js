d44a880a8dee2b179b01d3783b84bc5bf1a58512
ebubedan <142698157+ebubedan@users.noreply.github.com>
2023-12-11 09:38:38 -1200

    Add files via upload

---
+++ daniel ude.js
@@ -0,0 +1,65 @@
+/*function functionTool (a = 15, b = 12) {
+    return a + b;
+}
+console.log(functionTool());*/
+
+/*function Respested(){
+    x = 22;
+    y = 23;
+    return x * y;
+}
+console.log(Respested());
+
+function weapon() {
+    tony = "axe"
+    jude = "cutlass"
+    return "tony " + "jude ";
+}
+console.log(weapon());
+
+function Negro (a = " was a dark man, ", b = " was loved by everyone ") {
+    return a + b;
+}
+console.log(Negro());
+
+ function Dewee() {
+     return "made fun of people but was a nice looking guy ";
+    
+ }
+ console.log(Dewee());*/
+ 
+
+/*=== equal to
+!== not equal to
+|| or
+
+*/
+
+/*function(birthYear, currentYear){
+    age= currentYear-birthYear
+    
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
